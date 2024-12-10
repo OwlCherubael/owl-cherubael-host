8ba405cb8feb21caf8009ec6d32de4a975f8b3e8
OwlCherubael <95769237+OwlCherubael@users.noreply.github.com>
2024-12-10 16:11:30 +0300

    Fix routes

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// index.js
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Heroku!');
+    res.send('Hello, world!');
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
